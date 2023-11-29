2ffb4dd31ab7e2f66d5eb3cd4576e537cb028634
Agus <aguskenny24@gmail.com>
2023-11-29 18:10:50 -0300

    - Modified nav - Going to change person icon

--- src/components/FullNav.tsx
+++ src/components/FullNav.tsx
@@ -1,21 +1,25 @@
 import { navMenuItems } from "cms/NavMenu";
 import NavMenu from "./NavMenu";
 import Link from "next/link";
+import Auth from "./Auth";
 
 export default function FullNav() {
-    return (
-        <div className="p-5 sticky top-0 left-0 flex flex-row align-middle items-center w-full">
-            <NavMenu
-                className="mr-auto self-center place-self-center justify-self-center"
-                items={navMenuItems}
-            />
-            <Link
-                href="/"
-                target="_self"
-                className="ml-auto justify-self-start hover:text-light-accent dark:hover:text-dark-accent font-medium"
-            >
-                Chatrooms app
-            </Link>
-        </div>
-    );
+  return (
+    <div className="p-5 sticky top-0 left-0 flex flex-row align-middle items-center w-full">
+      <div className="flex flex-row gap-8 items-center h-full justify-center">
+        <NavMenu
+          className="mr-auto self-center place-self-center justify-self-center"
+          items={navMenuItems}
+        />
+        <Auth />
+      </div>
+      <Link
+        href="/"
+        target="_self"
+        className="ml-auto justify-self-start hover:text-light-accent dark:hover:text-dark-accent font-medium"
+      >
+        Chatrooms app
+      </Link>
+    </div>
+  );
 }
--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -4,92 +4,118 @@ import * as React from "react";
 import Link from "next/link";
 
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "fndtn/components/ui/navigation-menu";
 import { cn } from "fndtn/lib/utils";
 import { INavMenuItem } from "fndtn/interfaces/INavMenuItem";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
-export default function NavMenu(props: { items: INavMenuItem[]; className?:string }) {
-    const { items, className } = props;
-    return (
-        <NavigationMenu className={`w-full ${className || ""}`}>
-            {items.map((item, keyItem) => {
-                return (
-                    <NavigationMenuList key={item.trigger + keyItem}>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>
-                                {item.trigger}
-                            </NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[600px] ">
-                                    {item.content.map(
-                                        (contentItem, contentItemKey) => (
-                                            <ListItem
-                                                key={
-                                                    contentItem.link.display +
-                                                    contentItemKey
-                                                }
-                                                title={contentItem.link.display}
-                                                href={contentItem.link.href}
-                                                target={contentItem.link.target}
-                                            >
-                                                {contentItem.description}
-                                            </ListItem>
-                                        )
-                                    )}
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Link href="/help" legacyBehavior passHref>
-                                <NavigationMenuLink
-                                    className={navigationMenuTriggerStyle()}
-                                >
-                                    Help
-                                </NavigationMenuLink>
-                            </Link>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem className="border-l border-dotted">
-                            <ThemeSwitcher/>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                );
-            })}
-        </NavigationMenu>
-    );
+export default function NavMenu(props: {
+  items: INavMenuItem[];
+  className?: string;
+}) {
+  const { items, className } = props;
+  return (
+    <NavigationMenu
+      className={`w-full ${className || ""}`}
+    >
+      {items.map((item, keyItem) => {
+        return (
+          <NavigationMenuList
+            key={item.trigger + keyItem}
+          >
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>
+                {item.trigger}
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[600px] ">
+                  {item.content.map(
+                    (
+                      contentItem,
+                      contentItemKey
+                    ) => (
+                      <ListItem
+                        key={
+                          contentItem.link
+                            .display +
+                          contentItemKey
+                        }
+                        title={
+                          contentItem.link.display
+                        }
+                        href={
+                          contentItem.link.href
+                        }
+                        target={
+                          contentItem.link.target
+                        }
+                      >
+                        {contentItem.description}
+                      </ListItem>
+                    )
+                  )}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link
+                href="/help"
+                legacyBehavior
+                passHref
+              >
+                <NavigationMenuLink
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Help
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="border-l border-dotted">
+              <ThemeSwitcher />
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        );
+      })}
+    </NavigationMenu>
+  );
 }
 
 const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(
+  (
+    { className, title, children, ...props },
+    ref
+  ) => {
     return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent dark:hover:text-dark-accent hover:text-light-accent focus:bg-accent focus:text-accent-foreground bg-opacity-75",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">
-                        {title}
-                    </div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent dark:hover:text-dark-accent hover:text-light-accent focus:bg-accent focus:text-accent-foreground bg-opacity-75",
+              className
+            )}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">
+              {title}
+            </div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
     );
-});
+  }
+);
 ListItem.displayName = "ListItem";
